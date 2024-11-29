327cca9bfe674badce4c641f5fd3cfc1ad0626d6
Dmitriy M <ya-fhh@yandex.ru>
2024-11-29 23:40:11 +0300

    wip

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,14 +18,18 @@ class SiteController extends AbstractController
     }
 
     #[Route('/calculate-price', methods: ['POST'])]
-    public function calculatePrice(): JsonResponse
+    public function calculatePrice(Request $request): JsonResponse
     {
-        return $this->json(['error' => '', 'price' => 0], Response::HTTP_OK);
+		$data = json_decode($request->getContent(), true);
+
+        return $this->json(['error' => '', 'price' => 0, 'echo' => $data], Response::HTTP_OK);
     }
 
     #[Route('/purchase', methods: ['POST'])]
-    public function purchase(): JsonResponse
+    public function purchase(Request $request): JsonResponse
     {
-        return $this->json(['error' => '', 'purchase_id' => 0], Response::HTTP_OK);
+		$data = json_decode($request->getContent(), true);
+
+        return $this->json(['error' => '', 'purchase_id' => 0, 'echo' => $data], Response::HTTP_OK);
     }
 }
